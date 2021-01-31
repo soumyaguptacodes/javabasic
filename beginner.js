d539b61e1d81abb9382fbaf21bd2ff15ec0e47c5
Soumya Gupta <74004714+soumyaguptacodes@users.noreply.github.com>
2021-01-31 23:14:01 +0530

    this is javaprogram

---
+++ beginner.js
@@ -0,0 +1,113 @@
+//console.log(firstName+''+age)='john 
+//var yearsona = 2003 -  1;
+//console.log(yearsona);
+
+ //var year, yearsona, yearpp; 
+//year = 2005;
+//yearsona = year - 1
+//yearpp = year- 2
+ 
+//var firstNamemark = 'mark' ;
+//var massMark = 40;
+//var heightMark = 5 ;
+//BMI
+ //var markBMI = massMark/ heightMark*heightMark
+//console.log(markBMI);
+
+//var firstNamealice = 'alice' ;
+//var massMark = 44;
+//var heightMark = 3 ;
+//BMI
+ //var aliceBMI = massMark/ heightMark*heightMark
+//console.log("This is Mark's BMI",markBMI);
+
+
+
+
+
+
+/*var firstName = 'John';
+var age = 16;
+if (age < 13) {
+  console.log(firstName + 'is a boy');
+}
+else {
+  console.log(firstName + ' is a teenager');
+}
+
+
+
+var firstName = 'John';
+var age = 16;
+
+age >= 18? console.log(firstName+ 'drinks beer.')
+: console.log(firstName+ 'drinks juice.');*/
+
+
+/*var firstName = 'John';
+var job = 'instructor';
+switch (job){
+  case 'teacher':
+    console.log(firstName + 'teaches kids how to teach');
+    break;
+  default:
+    console.log(firstName + ' does something else');
+}
+
+
+
+
+var a = "2"
+
+// 1st way
+if (a == "8"){
+  console.log("print 2");
+  
+}
+
+else {
+  console.log("not 2");
+}
+
+// 2nd way
+
+a == "2" ? "print 2" : "not 2"*/
+
+
+// var firstName1 = "johns team";
+// console.log (firstName1);
+
+// var score1 = (89+120+130) /3 ;
+// console.log (score1);
+          
+
+
+
+// var firstName2 = "micke team"
+// console.log(firstName2);
+
+// var score2 = (89+100+30)/3 ;
+// console.log (score2);
+
+
+// if (score1>score2){
+//   console.log (firstName1+'jhons team is winner');
+// }
+// else 
+//   {
+//     console.log(firstName2+ "micke team is winner");
+// }
+                
+
+
+
+
+
+
+
+
+
+
+
+
+ 
